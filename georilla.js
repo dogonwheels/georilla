694ca60a6047d0d301b9d3ec6eacd369f56625bf
Dom Crayford <domcrayford@gmail.com>
2010-03-07 12:04:02 +0000

    Georilla patch works for safari - providing fake geo info

---
+++ georilla.js
@@ -0,0 +1,25 @@
+var georilla = {};
+georilla.punch = function () {
+    console.log('Patching geolocation with georilla');
+    navigator.geolocation = georilla.geolocation;
+};
+
+georilla.geolocation = {};
+georilla.geolocation.getCurrentPosition = function (successCallback, errorCallback, options) {
+    var position = {
+        coords: {
+            latitude: 200,
+            longitude: 100,
+            accuracy: 50,
+            altitude: 0,
+            altitudeAccuracy: 100,
+            speed: -1,
+            heading: 10,
+        }
+    };
+    var success = function () {
+        successCallback(position);
+    };
+
+    setTimeout(success, 1000);
+};
