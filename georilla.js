3da5b7105ceb52957d92ea50f6dff485e49b58e7
Dom Crayford <domcrayford@gmail.com>
2010-03-08 18:34:49 +0000

    Added test framework when realising as adding FIXMEs that, for a test helper, this is complicated enough to warrent some tests :)

--- georilla.js
+++ georilla.js
@@ -5,6 +5,12 @@ georilla.punch = function () {
 };
 
 georilla.geolocation = {};
+
+/*
+   void getCurrentPosition(in PositionCallback successCallback,
+                           in optional PositionErrorCallback errorCallback,
+                           in optional PositionOptions options);
+*/
 georilla.geolocation.getCurrentPosition = function (successCallback, errorCallback, options) {
     var position = {
         coords: {
@@ -17,9 +23,43 @@ georilla.geolocation.getCurrentPosition = function (successCallback, errorCallba
             heading: 10,
         }
     };
+    // Always succeed for now
+    // FIXME: Need to allow for errors
+    // FIXME: Redirect position to a cursor (runner) and device for introducing
+    // errors
     var success = function () {
         successCallback(position);
     };
 
     setTimeout(success, 1000);
 };
+
+/*
+   long watchPosition(in PositionCallback successCallback,
+                      in optional PositionErrorCallback errorCallback,
+                      in optional PositionOptions options);
+*/
+
+/*
+   void clearWatch(in long watchId);
+*/
+
+/*
+ [Callback=FunctionOnly, NoInterfaceObject]
+ interface PositionCallback {
+   void handleEvent(in Position position);
+ };
+
+ [Callback=FunctionOnly, NoInterfaceObject]
+ interface PositionErrorCallback {
+   void handleEvent(in PositionError error);
+ };
+
+ [Callback, NoInterfaceObject]
+  interface PositionOptions {
+    attribute boolean enableHighAccuracy;
+    attribute long timeout;
+    attribute long maximumAge;
+  };
+
+*/
