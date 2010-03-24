af2d3f4431b71e034d125bd448e203ace2deae3a
Dom Crayford <domcrayford@gmail.com>
2010-03-24 19:13:36 +0000

    Fixed error codes to numbers and removed excess API comments in georilla

--- georilla.js
+++ georilla.js
@@ -15,46 +15,10 @@ georilla.newGeolocation = function () {
     var geo = {
         currentTime: 0,
         nextWatchId: 0,
-        watchSuccessCallbacks: {}
-    };
-    /*
-       void getCurrentPosition(in PositionCallback successCallback,
-                               in optional PositionErrorCallback errorCallback,
-                               in optional PositionOptions options);
-    */
-    geo.getCurrentPosition = function (successCallback, errorCallback, options) {
-        var maximumAge = 0;
-        if (exists(options) && exists(options.maximumAge)) {
-            maximumAge = options.maximumAge;
-        }
-
-        if (exists(geo.currentPosition) && 
-            (geo.currentTime - geo.currentPosition.timestamp) <= maximumAge) {
-            // We have a cached position, callback straightaway
-            successCallback(geo.currentPosition);
-            return;
-        }
-
-        if (exists(options) && exists(options.timeout)) {
-            var timeout = options.timeout;
-            if (timeout <= 0) {
-                errorCallback({code: 'TIMEOUT'});
-                return;
-            }
-        }
-
-        // Currently don't have any positions, but we should callback when
-        // we do get one
-        geo.successCallback = successCallback;            
-        
-        if (exists(timeout)) {
-            var onTimeout = function () {
-                delete geo.successCallback;
-                errorCallback({code: 'TIMEOUT'});
-            };
-            setTimeout(onTimeout, timeout);
-        }
-
+        watchSuccessCallbacks: {},
+        PERMISSION_DENIED: 1,
+        POSITION_UNAVAILABLE: 2,
+        TIMEOUT: 3
     };
 
     geo.setCurrentPosition = function (position) {
@@ -82,11 +46,41 @@ georilla.newGeolocation = function () {
         geo.currentTime = timestamp;
     };
 
-    /*
-       long watchPosition(in PositionCallback successCallback,
-                          in optional PositionErrorCallback errorCallback,
-                          in optional PositionOptions options);
-    */
+    geo.getCurrentPosition = function (successCallback, errorCallback, options) {
+        var maximumAge = 0;
+        if (exists(options) && exists(options.maximumAge)) {
+            maximumAge = options.maximumAge;
+        }
+
+        if (exists(geo.currentPosition) && 
+            (geo.currentTime - geo.currentPosition.timestamp) <= maximumAge) {
+            // We have a cached position, callback straightaway
+            successCallback(geo.currentPosition);
+            return;
+        }
+
+        if (exists(options) && exists(options.timeout)) {
+            var timeout = options.timeout;
+            if (timeout <= 0) {
+                errorCallback({code: geo.TIMEOUT});
+                return;
+            }
+        }
+
+        // Currently don't have any positions, but we should callback when
+        // we do get one
+        geo.successCallback = successCallback;            
+        
+        if (exists(timeout)) {
+            var onTimeout = function () {
+                delete geo.successCallback;
+                errorCallback({code: geo.TIMEOUT});
+            };
+            setTimeout(onTimeout, timeout);
+        }
+
+    };
+
     geo.watchPosition = function (successCallback, errorCallback, options) {
         geo.nextWatchId++;
 
@@ -107,7 +101,7 @@ georilla.newGeolocation = function () {
         var startTimeout = function () {
             var onError = function () {
                 if (exists(errorCallback)) {
-                    errorCallback( { code:'TIMEOUT' } );
+                    errorCallback( { code: geo.TIMEOUT } );
                 }
                 startTimeout();
             };
@@ -129,55 +123,10 @@ georilla.newGeolocation = function () {
         return geo.nextWatchId;
     };
 
-    /*
-       void clearWatch(in long watchId);
-    */
     geo.clearWatch = function (watchId) {
         geo.watchSuccessCallbacks[watchId].resetTimeout();
         delete(geo.watchSuccessCallbacks[watchId]);
     }
 
-    /*
-     [Callback=FunctionOnly, NoInterfaceObject]
-     interface PositionCallback {
-       void handleEvent(in Position position);
-     };
-
-     [Callback=FunctionOnly, NoInterfaceObject]
-     interface PositionErrorCallback {
-       void handleEvent(in PositionError error);
-     };
-
-     [Callback, NoInterfaceObject]
-      interface PositionOptions {
-        attribute boolean enableHighAccuracy;
-        attribute long timeout;
-        attribute long maximumAge;
-      };
-
-      interface Position {
-        readonly attribute Coordinates coords;
-        readonly attribute DOMTimeStamp timestamp;
-      };
-      
-      interface Coordinates {
-        readonly attribute double latitude;
-        readonly attribute double longitude;
-        readonly attribute double? altitude;
-        readonly attribute double accuracy;
-        readonly attribute double? altitudeAccuracy;
-        readonly attribute double? heading;
-        readonly attribute double? speed;
-      };
-      
-      interface PositionError {
-        const unsigned short PERMISSION_DENIED = 1;
-        const unsigned short POSITION_UNAVAILABLE = 2;
-        const unsigned short TIMEOUT = 3;
-        readonly attribute unsigned short code;
-        readonly attribute DOMString message;
-      };
-      
-    */
     return geo;
 };
